99ce333a1fa0c3244691481bce6b20e6fe92c3e6
Gert Van Gool <gertvangool@gmail.com>
2010-02-12 15:50:25 +0100

    Added simple setup.py and version info

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+import storage_migration
+
+setup(name='django-storage-migration',
+      version='.'.join(map(str, storage_migration.__version__)),
+      description=storage_migration.__doc__,
+      author='City Live NV',
+      author_email='gert.vangool@citylive.be',
+      url='http://github.com/citylive/django-storage-migration/',
+      packages=['storage_migration'],
+      license='BSD',
+      include_package_data = True,
+      zip_safe = False,
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+      ],
+)
+
---
+++ storage_migration/__init__.py
@@ -0,0 +1,2 @@
+'''A simple way to migrate between Django's storage backends'''
+__version__ = (0, 1, 0)
