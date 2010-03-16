6ef60a7c2f8171416b2bcaec8284de3befb288cd
Gert Van Gool <gertvangool@gmail.com>
2010-03-16 13:02:37 +0100

    Added support for moving from current storage to a new storage. This allows you to migrate the storage backends without taking your files offline.

--- storage_migration/__init__.py
+++ storage_migration/__init__.py
@@ -1,2 +1,2 @@
 '''A simple way to migrate between Django's storage backends'''
-__version__ = (0, 2, 1)
+__version__ = (0, 3, 0)
--- storage_migration/management/commands/migrate_storage.py
+++ storage_migration/management/commands/migrate_storage.py
@@ -11,12 +11,23 @@ OLD_DEFAULT_FILE_STORAGE = getattr(settings, 'OLD_DEFAULT_FILE_STORAGE', default
 if isinstance(OLD_DEFAULT_FILE_STORAGE, str):
     OLD_DEFAULT_FILE_STORAGE = get_storage_class(OLD_DEFAULT_FILE_STORAGE)()
 
+NEW_STORAGE = getattr(settings, 'NEW_STORAGE', {})
+NEW_DEFAULT_FILE_STORAGE = getattr(settings, 'NEW_DEFAULT_FILE_STORAGE', default_storage)
+if isinstance(NEW_DEFAULT_FILE_STORAGE, str):
+    NEW_DEFAULT_FILE_STORAGE = get_storage_class(NEW_DEFAULT_FILE_STORAGE)()
+
+NEW_STORAGE = getattr(settings, 'NEW_STORAGE', {})
+NEW_DEFAULT_FILE_STORAGE = getattr(settings, 'NEW_DEFAULT_FILE_STORAGE', default_storage)
+if isinstance(NEW_DEFAULT_FILE_STORAGE, str):
+    NEW_DEFAULT_FILE_STORAGE = get_storage_class(NEW_DEFAULT_FILE_STORAGE)()
+
 class Command(LabelCommand):
     args = '<app_name.Model app_name.Model2 ...>'
     label = 'model (app_name.ModelName)'
     help = 'Migrate all the FileFields on a given Model to a new Storage backend'
     option_list = LabelCommand.option_list + (
         make_option('--overwrite', '-f', action='store_true', dest='overwrite', help='Overwrite file that exist in the new storage backend'),
+        make_option('--to-new', action='store_true', dest='to_new', help='Copy files from the current storage backend to the new storage backend'),
     )
 
     def handle_label(self, label, **options):
@@ -30,16 +41,28 @@ class Command(LabelCommand):
             if isinstance(field, FileField):
                 field_names.append(field.name)
                 field_path = '%s.%s' % (label, field.name)
-                if field_path in OLD_STORAGE:
-                    old_storages[field_path] = OLD_STORAGE[field_path]
+                if options['to_new']:
+                    if field_path in NEW_STORAGE:
+                        old_storages[field_path] = NEW_STORAGE[field_path]
+                    else:
+                        old_storages[field_path] = NEW_DEFAULT_FILE_STORAGE
                 else:
-                    old_storages[field_path] = OLD_DEFAULT_FILE_STORAGE
+                    if field_path in OLD_STORAGE:
+                        old_storages[field_path] = OLD_STORAGE[field_path]
+                    else:
+                        old_storages[field_path] = OLD_DEFAULT_FILE_STORAGE
         for instance in model_class._default_manager.all():
             logging.debug('Handling "%s"' % instance)
             # check all field names
             for fn in field_names:
                 field = getattr(instance, fn)
-                old_storage = old_storages['%s.%s' % (label, fn)]
+                if options['to_new']:
+                    new_storage = old_storages['%s.%s' % (label, fn)]
+                    old_storage = field.storage
+                else:
+                    old_storage = old_storages['%s.%s' % (label, fn)]
+                    new_storage = field.storage
+
                 if field.name == '':
                     logging.debug('Field is empty, ignoring file.')
                 elif field.storage == old_storage:
